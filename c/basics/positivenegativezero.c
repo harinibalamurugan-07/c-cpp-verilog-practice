c0499accc2d346fb5e085ec83d65a8b544cf3628
harinibalamurugan-07 <harinibalamurugan2004@gmail.com>
2025-12-17 19:21:26 +0530

    Create positivenegativezero.c

---
+++ c/basics/positivenegativezero.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    (a>0)?printf("%d is postive",a):(a<0)?printf("%d is negative",a):printf("%d is zero",a);
+        
+    
+}
