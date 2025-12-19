e0b386e759e4b3112a52671b699b0e94e6d4ceaf
harinibalamurugan-07 <harinibalamurugan2004@gmail.com>
2025-12-19 21:57:20 +0530

    Create printing1ton.c

---
+++ c/basics/printing1ton.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+//using for-loop
+//int main() {
+  // int n;
+   //printf("Enter the number:");
+   //scanf("%d",&n);
+   //for(int i=0;i<=n;i++){
+     //  printf("%d\n",i);
+   //}
+//}
+//using while-loop
+//int main(){
+//    int n;
+//    printf("Enter the number:");
+//    scanf("%d",&n);
+//  int i=0;
+//  while(i<=n){
+//       printf("%d\n",i);
+//       i++;
+//    }
+//}
+int main(){
+     int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    int i=0;
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i<=n);
+    
+}
