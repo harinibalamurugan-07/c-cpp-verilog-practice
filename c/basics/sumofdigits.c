70930e3a33bd26e17ceac33c07dc724914c6a1c3
harinibalamurugan-07 <harinibalamurugan2004@gmail.com>
2025-12-21 21:17:25 +0530

    Create sumofdigits.c

---
+++ c/basics/sumofdigits.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    int n,sum=0;;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while(n!=0){
+        sum=sum+(n%10);
+        n=n/10;
+        
+    }
+printf("sum of digits:%d",sum);
+    return 0;
+}
