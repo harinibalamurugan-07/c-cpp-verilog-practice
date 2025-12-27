de8d1a6846e8eda75d1117b501278f9af9c1fcb5
harinibalamurugan-07 <harinibalamurugan2004@gmail.com>
2025-12-27 20:39:47 +0530

    Create largest_element_1D_array.c

---
+++ c/week-2/largest_element_1D_array.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main(){
+    int n;
+    int a[100];
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+       printf("Enter %d element:",i);
+      scanf("%d",&a[i]); 
+    }
+    int max=a[0];
+    for(int i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+    }
+    printf("Largest element:%d",max);
+    return 0;
+}
