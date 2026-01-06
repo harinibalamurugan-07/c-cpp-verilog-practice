d87eaeceafae4b5051168517d0e21a3ea6f34608
harinibalamurugan-07 <harinibalamurugan2004@gmail.com>
2026-01-06 20:01:12 +0530

    Create null_pointer.c

---
+++ c/week3/null_pointer.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int *p=NULL;
+    if(p==NULL){
+        printf("NULL pointer");
+    }else{
+        printf("Memory allocated");
+    }
+
+    return 0;
+}
